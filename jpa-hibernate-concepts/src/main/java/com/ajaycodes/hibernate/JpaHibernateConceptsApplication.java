776db7d284fd08809c03c5f080b70009520a741d
Ajay Kumar <ajay_kumar_1990@live.com>
2023-01-09 11:19:35 +0530

    Initial commit

---
+++ jpa-hibernate-concepts/src/main/java/com/ajaycodes/hibernate/JpaHibernateConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.ajaycodes.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateConceptsApplication.class, args);
+	}
+
+}
---
+++ jpa-hibernate-concepts/src/test/java/com/ajaycodes/hibernate/JpaHibernateConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajaycodes.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
